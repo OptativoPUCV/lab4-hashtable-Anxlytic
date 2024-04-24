0be451c677d4a3d359d20591ce7a7b6af63e3640
Anxlytic <163349868+Anxlytic@users.noreply.github.com>
2024-04-24 21:14:28 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -107,7 +107,6 @@ void eraseMap(HashMap * map,  char * key)
   {
     while(map->buckets[posicion]!=NULL && map->buckets[posicion]->key!=NULL)
       {
-        posicion++;
         if(is_equal(map->buckets[posicion]->key,key))
         {
           map->buckets[posicion]->key=NULL;
